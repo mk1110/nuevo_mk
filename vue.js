a855c60979a8d828afd4f48a4c194f684138c423
Mariano <mk1110aballay@gmail.com>
2024-07-16 22:26:55 -0300

    nuevosarreglos

---
+++ vue.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue
+createApp({
+ data() {
+ return {
+ saludo: 'Hola a Todos!!!',
+ nombre: 'Kevin',
+ apellido:'Aballay',
+ provincia:'San Juan',
+ edad: '31',
+ tecnologias:['HTML', 'CSS', 'JAVASCRIPT', 'PYTHON','MYSQL','VUE','GIT','GITHub']
+ }
+ }
+}).mount('#app')
